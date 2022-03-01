e0471da35a9674465271d051d55212492bd9dbca
Sergei Chernov <sergey.v.chernov@yandex.ru>
2022-03-01 21:35:27 +0300

    1.1. Создание объекта [#165452]

---
+++ src/main/java/ru/job4j/tracker/oop/Dog.java
@@ -0,0 +1,10 @@
+package ru.job4j.tracker.oop;
+
+public class Dog {
+    public static void main(String[] args) {
+        Dog sharik = new Dog();
+        Dog polkan = new Dog();
+        Dog zhuchka = new Dog();
+
+    }
+}
