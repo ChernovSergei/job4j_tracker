c04a20893259697a28b792c8f8b7a7806379cd9b
Sergei Chernov <sergey.v.chernov@yandex.ru>
2022-03-04 19:01:37 +0300

    5. Аннотация @Override [#174891]

--- src/main/java/ru/job4j/tracker/inheritance/ReportUsage.java
+++ src/main/java/ru/job4j/tracker/inheritance/ReportUsage.java
@@ -4,11 +4,14 @@ public class ReportUsage {
     public static void main(String[] args) {
         TextReport report = new TextReport();
         HTMLReport reportHTML = new HTMLReport();
+        JSONReport reportJSON = new JSONReport();
         String name = "name";
         String body = "body";
         String text = report.generate(name, body);
         System.out.println(text);
         text = reportHTML.generate(name, body);
+        System.out.println(text);
+        text = reportJSON.generate(name, body);
         System.out.println(text);
     }
 }
