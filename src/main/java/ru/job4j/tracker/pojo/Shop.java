9724e4eb06f22fcc8a27c9bb8cfa279f08f781f1
Sergei Chernov <sergey.v.chernov@yandex.ru>
2022-03-04 23:29:03 +0300

    3.1. Массив с пустыми ячейками [#176470]

---
+++ src/main/java/ru/job4j/tracker/pojo/Shop.java
@@ -0,0 +1,30 @@
+package ru.job4j.tracker.pojo;
+
+public class Shop {
+    public static int indexOfNull(Product[] products) {
+        int result = -1;
+        for (int i = 0; i < products.length; i++) {
+            if (products[i] == null) {
+                result = i;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static void print(Product[] products) {
+        for (int i = 0; i < products.length; i++) {
+            if (products[i] != null) {
+                System.out.println(products[i].getName());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Product[] products = new Product[5];
+        products[0] = new Product("Milk", 10);
+        products[1] = new Product("Bread", 4);
+        products[2] = new Product("Egg", 19);
+        print(products);
+    }
+}
