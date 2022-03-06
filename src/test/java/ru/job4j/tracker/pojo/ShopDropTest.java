5f8aaaa1ba65dbc7ace4467c629053573b0dab67
Sergei Chernov <sergey.v.chernov@yandex.ru>
2022-03-06 08:19:19 +0300

    3.2. Удаление моделей из массива. [#310016 #272172]

--- src/test/java/ru/job4j/tracker/pojo/ShopDropTest.java
+++ src/test/java/ru/job4j/tracker/pojo/ShopDropTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
-//import static org.junit.Assert.*;
 
 public class ShopDropTest {
     @Test
