e46e43ce3a06b60b56eb84173f39cd2167a9b432
Sergei Chernov <sergey.v.chernov@yandex.ru>
2022-03-08 22:54:59 +0300

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -10,5 +10,7 @@ public class StartUI {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String date = created.format(formatter);
         System.out.println(date);
+        System.out.println();
+        System.out.println(item);
     }
 }
