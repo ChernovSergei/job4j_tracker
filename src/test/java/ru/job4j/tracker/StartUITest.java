3affd9364acdcee2b39a57a1fcdae3f67562905b
Sergei Chernov <sergey.v.chernov@yandex.ru>
2022-03-12 15:28:43 +0300

    9.2. Тесты вывода на консоль [#33585]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -12,7 +12,7 @@ public class StartUITest {
         Input input = new StubInput(answers);
         Output output = new StubOutput();
         Tracker tracker = new Tracker();
-        UserAction[] action = {new CreateAction(output), new ExitAction()};
+        UserAction[] action = {new CreateAction(output), new ExitAction(output)};
         new StartUI(output).init(input, tracker, action);
         assertThat(tracker.findAll()[0].getName(), is("Create Item"));
     }
@@ -25,7 +25,7 @@ public class StartUITest {
         Input input = new StubInput(answers);
         Output output = new StubOutput();
         tracker.add(item);
-        UserAction[] action = {new ReplaceAction(output), new ExitAction()};
+        UserAction[] action = {new ReplaceAction(output), new ExitAction(output)};
         new StartUI(output).init(input, tracker, action);
         assertThat(tracker.findById(item.getId()).getName(), is("Replaced Item"));
     }
@@ -38,8 +38,32 @@ public class StartUITest {
         Tracker tracker = new Tracker();
         Item item = new Item("Item to delete");
         tracker.add(item);
-        UserAction[] action = {new DeleteAction(output), new ExitAction()};
+        UserAction[] action = {new DeleteAction(output), new ExitAction(output)};
         new StartUI(output).init(input, tracker, action);
         assertThat(tracker.findById(item.getId()), is(nullValue()));
+    }
+
+    @Test
+    public void whenReplaceItemTestOutputIsSuccessfully() {
+        Item item = new Item("Item to replace");
+        Tracker tracker = new Tracker();
+        tracker.add(item);
+        String[] answers = {"0", String.valueOf(item.getId()), "Replaced item", "1"};
+        Input input = new StubInput(answers);
+        Output out = new StubOutput();
+        UserAction[] actions = {new ReplaceAction(out), new ExitAction(out)};
+        new StartUI(out).init(input, tracker, actions);
+        String ln = System.lineSeparator();
+        assertThat(out.toString(), is(
+                "Menu:"
+                + "0. Replace item."
+                + "1. Exit."
+                + "== Replace Item =="
+                + "The request had been updated successfully"
+                + "Menu:"
+                + "0. Replace item."
+                + "1. Exit."
+                + "== Exit =="
+        ));
     }
 }
