aa61ecf9f395b4fca16a23928da3016cb0081f5f
Sergei Chernov <sergey.v.chernov@yandex.ru>
2022-03-13 20:10:13 +0300

    9.1 Зависимость от System.out [#33568 #274323]

--- src/main/java/ru/job4j/tracker/ConsoleInput.java
+++ src/main/java/ru/job4j/tracker/ConsoleInput.java
@@ -3,17 +3,12 @@ package ru.job4j.tracker;
 import java.util.Scanner;
 
 public class ConsoleInput implements Input {
-    private final Output out;
-
-    public ConsoleInput(Output out) {
-        this.out = out;
-    }
 
     private Scanner scanner = new Scanner(System.in);
 
     @Override
     public String askStr(String question) {
-        out.println(question);
+        System.out.println(question);
         return scanner.nextLine();
     }
 
