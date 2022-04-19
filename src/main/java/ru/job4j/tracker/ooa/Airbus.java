56e4bcaafd75fdd25f9f125f4d5589f248661fb5
Sergei Chernov <sergey.v.chernov@yandex.ru>
2022-04-20 00:21:41 +0800

    2. final [#334073]

---
+++ src/main/java/ru/job4j/tracker/ooa/Airbus.java
@@ -0,0 +1,33 @@
+package ru.job4j.tracker.ooa;
+
+public final class Airbus extends Aircraft {
+    private static final int COUNT_ENGINE = 2;
+    private String name;
+
+    public Airbus(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void printModel() {
+        System.out.println("Engine model: " + name);
+    }
+
+    public void printCountEngine() {
+        int engines = (this.getName().equals("A380")) ? (4) : (COUNT_ENGINE);
+        System.out.println("Engine count = " + engines);
+    }
+
+    @Override
+    public String toString() {
+        return "Airbus{ name'" + name + '\'' + '}';
+    }
+}
---
+++ src/main/java/ru/job4j/tracker/ooa/Aircraft.java
@@ -0,0 +1,9 @@
+package ru.job4j.tracker.ooa;
+
+public class Aircraft {
+    protected void printModel() {
+        System.out.println("The method print the aircraft model");
+    }
+}
+
+
