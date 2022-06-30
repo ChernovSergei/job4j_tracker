c190ecbc2e4aba945d73161e981481cb8c0a54ea
ChernovSergei <sergey.v.chernov@yandex.ru>
2022-06-30 22:46:26 +0800

    3. Модели данных и HashSet. [#218530 #316375]

--- src/main/java/ru/job4j/tracker/collection/NotifyAccount.java
+++ src/main/java/ru/job4j/tracker/collection/NotifyAccount.java
@@ -7,9 +7,7 @@ public class NotifyAccount {
     public static HashSet<Account> sent(List<Account> accounts) {
         HashSet<Account> rsl = new HashSet<>();
         for (Account account : accounts) {
-            if (!account.equals(rsl)) {
-                rsl.add(account);
-            }
+            rsl.add(account);
         }
         return rsl;
     }
