56bb776bde72b9353b9a2e9122c9e2d788ed99cc
ChernovSergei <sergey.v.chernov@yandex.ru>
2022-07-09 22:01:27 +0800

    3. Банковские переводы [#10038]

--- src/main/java/ru/job4j/tracker/bank/BankService.java
+++ src/main/java/ru/job4j/tracker/bank/BankService.java
@@ -16,71 +16,58 @@ public class BankService {
 
     public void addAccount(String passport, Account account) {
         boolean noAccount = true;
-        User userLink = null;
-        for (Map.Entry<User, List<Account>> entry : users.entrySet()) {
-            if (entry.getKey().getPassport().equals(passport)) {
-                userLink = entry.getKey();
-                for (Account iterator : entry.getValue()) {
-                    if (iterator == account) {
-                        noAccount = false;
-                        break;
-                    }
-                }
+        User user = findByPassport(passport);
+        for (Account accountIterator : users.get(user)) {
+            if (accountIterator.equals(account)) {
+               noAccount = false;
             }
         }
         if (noAccount) {
-            users.get(userLink).add(account);
+            users.get(user).add(account);
         }
     }
 
     public User findByPassport(String passport) {
         User user = null;
-        for (Map.Entry<User, List<Account>> entry : users.entrySet()) {
-            if (entry.getKey().getPassport().equals(passport)) {
-                user = entry.getKey();
+        for (User userIterator : users.keySet()) {
+            if (userIterator.getPassport().equals(passport)) {
+                user = userIterator;
             }
         }
         return user;
     }
 
     public Account findByRequisite(String passport, String requisite) {
-        Account account = null;
-        for (Map.Entry<User, List<Account>> entry : users.entrySet()) {
-            if (entry.getKey().getPassport().equals(passport)) {
-                for (Account iterator : entry.getValue()) {
-                    if (iterator.getRequisite().equals(requisite)) {
-                        account = iterator;
-                    }
+        Account result = null;
+        User user = findByPassport(passport);
+        if (user != null) {
+            for (Account account : users.get(user)) {
+                if (account.getRequisite().equals(requisite)) {
+                    result = account;
                 }
             }
         }
-        return account;
+        return result;
     }
 
     public boolean transferMoney(String sourcePassport, String sourceRequisite,
                                  String destPassport, String destRequisite, double amount) {
-        boolean result1 = false;
-        boolean result2 = false;
-        for (Map.Entry<User, List<Account>> entry : users.entrySet()) {
-            if (entry.getKey().getPassport().equals(sourcePassport)) {
-                for (Account iterator : entry.getValue()) {
-                    if (iterator.getRequisite().equals(sourceRequisite) && iterator.getBalance() >= amount) {
-                        iterator.setBalance(iterator.getBalance() - amount);
-                        result1 = true;
-                    }
-                }
-            }
+        boolean result = false;
+        User userSource = findByPassport(sourcePassport);
+        Account accountSource = findByRequisite(sourcePassport, sourceRequisite);
+        users.putIfAbsent(findByPassport(destPassport), new ArrayList<Account>());
+        User userDest = findByPassport(destPassport);
+        Account accountDest = findByRequisite(destPassport, destRequisite);
+        if (accountDest.equals(null)) {
+            users.get(findByPassport(destPassport)).add(new Account(destRequisite, 0D));
         }
-        for (Map.Entry<User, List<Account>> entry : users.entrySet()) {
-            if (entry.getKey().getPassport().equals(destPassport)) {
-                for (Account iterator : entry.getValue()) {
-                    if (iterator.getRequisite().equals(destRequisite)) {
-                        iterator.setBalance(iterator.getBalance() + amount);
-                        result2 = true;
-                    }
-                }
-            }
+
+        if (!userSource.equals(null) && !accountSource.equals(null) &&  accountSource.getBalance() >= amount) {
+            accountSource.setBalance(accountSource.getBalance() - amount);
+            accountDest.setBalance(accountDest.getBalance() + amount);
+            result = true;
         }
-        return result1 && result2;
+
+        return result;
     }
 }
