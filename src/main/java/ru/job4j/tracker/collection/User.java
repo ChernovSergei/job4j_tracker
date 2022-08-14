113d309328dc569526322c62b80d9b8f90b0beb6
ChernovSergei <sergey.v.chernov@yandex.ru>
2022-08-14 18:35:56 +0300

    1. Организовать сортировку User   [#10034 #330900]

--- src/main/java/ru/job4j/tracker/collection/User.java
+++ src/main/java/ru/job4j/tracker/collection/User.java
@@ -13,13 +13,8 @@ public class User implements Comparable<User> {
 
     @Override
     public int compareTo(User o) {
-        if (name.compareTo(o.name) < 0) {
-            return -1;
-        }
-        if (name.compareTo(o.name) == 0) {
-            return Integer.compare(age, o.age);
-        }
-        return 1;
+        int rsl = name.compareTo(o.name);
+        return rsl != 0 ? rsl : Integer.compare(age, o.age);
     }
 
     @Override
