0af4463e2d50b5427fbefc03c664af26dccf17cb
Sergei Chernov <sergey.v.chernov@yandex.ru>
2022-09-02 16:22:12 +0300

    4. Сортировка номера [#221284]

---
+++ src/main/java/ru/job4j/tracker/collection/LexSort.java
@@ -0,0 +1,34 @@
+package ru.job4j.tracker.collection;
+
+import java.util.Comparator;
+
+public class LexSort implements Comparator<String> {
+
+    public static boolean isNumertic(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+    @Override
+    public int compare(String left, String right) {
+        String[] splitLeft = left.split("\\.");
+        String[] splitRight = right.split("\\.");
+        int result = 0;
+        for (int i = 0; i < splitLeft.length; i++) {
+            if (isNumertic(splitLeft[i]) && isNumertic(splitRight[i])) {
+                result = Integer.compare(Integer.parseInt(splitLeft[i]), Integer.parseInt(splitRight[i]));
+            } else if (isNumertic(splitLeft[i])) {
+                result = 1;
+            } else if (isNumertic(splitRight[i])) {
+                result = -1;
+            }
+            if (result != 0) {
+                return result;
+            }
+        }
+        return result;
+    }
+}
