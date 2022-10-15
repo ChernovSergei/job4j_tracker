62c492c43ce12017ba0c59dd257cbd9b0607df33
Sergei Chernov <sergey.v.chernov@yandex.ru>
2022-10-15 20:57:03 +0300

    1.1. Анонимные классы [#214142 #351871]

--- src/main/java/ru/job4j/tracker/lambda/AttachmentSort.java
+++ src/main/java/ru/job4j/tracker/lambda/AttachmentSort.java
@@ -21,12 +21,10 @@ public class AttachmentSort {
         };
         attachments.sort(comparator);
         System.out.println(attachments);
-        Comparator nameComparator = new Comparator() {
+        Comparator<Attachment> nameComparator = new Comparator<Attachment>() {
             @Override
-            public int compare(Object o1, Object o2) {
-                Attachment left = (Attachment) o1;
-                Attachment right = (Attachment) o2;
-                return left.getName().compareTo(right.getName());
+            public int compare(Attachment o1, Attachment o2) {
+                return o1.getName().compareTo(o2.getName());
             }
         };
     }
