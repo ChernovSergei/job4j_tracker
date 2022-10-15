99ebba9cbadb64b3b6075027807f4b5a1a73f811
Sergei Chernov <sergey.v.chernov@yandex.ru>
2022-10-15 11:05:32 +0300

    2. Отсортировать департаменты [#34352 #351702]

--- src/main/java/ru/job4j/tracker/collection/DepDescComp.java
+++ src/main/java/ru/job4j/tracker/collection/DepDescComp.java
@@ -7,17 +7,7 @@ public class DepDescComp implements Comparator<String> {
     public int compare(String o1, String o2) {
         String[] el1 = o1.split("/");
         String[] el2 = o2.split("/");
-        int lenghtMin = Math.min(el1.length, el2.length);
-        int result = 0;
-
-        for (int i = 0; i < lenghtMin; i++) {
-            if (i == 0) {
-                result = el2[i].compareTo(el1[i]);
-            } else if (result == 0) {
-                result = el1[i].compareTo(el2[i]);
-            }
-        }
-
-        return result != 0 ? result : ((Integer) el1.length).compareTo((Integer) el2.length);
+        int result = el2[0].compareTo(el1[0]);
+        return result != 0 ? result : o1.compareTo(o2);
     }
 }
