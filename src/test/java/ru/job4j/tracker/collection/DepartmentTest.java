435461340cb1cfd68790c2393cc3b280321554d3
Sergei Chernov <sergey.v.chernov@yandex.ru>
2022-10-28 20:57:03 +0300

    1. Метод .of [#70623]

--- src/test/java/ru/job4j/tracker/collection/DepartmentTest.java
+++ src/test/java/ru/job4j/tracker/collection/DepartmentTest.java
@@ -8,23 +8,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class DepartmentTest {
     @Test
     public void whenMissed() {
-        List<String> input = Arrays.asList("k1/sk1", "k2/sk1");
-        List<String> expected = Arrays.asList("k1", "k1/sk1", "k2", "k2/sk1");
+        List<String> input = List.of("k1/sk1", "k2/sk1");
+        List<String> expected = List.of("k1", "k1/sk1", "k2", "k2/sk1");
         List<String> result = Departments.fillGaps(input);
         assertThat(result).containsAll(expected);
     }
 
     @Test
     public void whenNonChange() {
-        List<String> input = Arrays.asList("k1", "k1/sk1");
-        List<String> expected = Arrays.asList("k1", "k1/sk1");
+        List<String> input = List.of("k1", "k1/sk1");
+        List<String> expected = List.of("k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         assertThat(result).containsAll(expected);
     }
 
     @Test
     public void whenSortAscWithoutMissedDepartments() {
-        List<String> input = Arrays.asList(
+        List<String> input = List.of(
                 "K1/SK1",
                 "K1/SK1/SSK1",
                 "K1/SK1/SSK2",
@@ -35,7 +35,7 @@ public class DepartmentTest {
                 "K2/SK1/SSK2",
                 "K2/SK1/SSK1"
         );
-        List<String> expected = Arrays.asList(
+        List<String> expected = List.of(
                 "K1",
                 "K1/SK1",
                 "K1/SK1/SSK1",
@@ -52,7 +52,7 @@ public class DepartmentTest {
 
     @Test
     public void whenSortAscWithMissedDepartments() {
-        List<String> input = Arrays.asList(
+        List<String> input = List.of(
                 "K1/SK1/SSK1",
                 "K1/SK1/SSK2",
                 "K2/SK1",
@@ -60,7 +60,7 @@ public class DepartmentTest {
                 "K2/SK1/SSK2",
                 "K2/SK1/SSK1"
         );
-        List<String> expected = Arrays.asList(
+        List<String> expected = List.of(
                 "K1/SK1/SSK1",
                 "K1/SK1/SSK2",
                 "K1/SK2",
@@ -74,7 +74,7 @@ public class DepartmentTest {
 
     @Test
     public void whenSortDescWithoutMissedDepartments() {
-        List<String> input = Arrays.asList(
+        List<String> input = List.of(
                 "K1/SK1",
                 "K1/SK1/SSK1",
                 "K1/SK1/SSK2",
@@ -85,7 +85,7 @@ public class DepartmentTest {
                 "K2/SK1/SSK2",
                 "K2/SK1/SSK1"
         );
-        List<String> expected = Arrays.asList(
+        List<String> expected = List.of(
                 "K2",
                 "K2/SK1",
                 "K2/SK1/SSK1",
@@ -102,7 +102,7 @@ public class DepartmentTest {
 
     @Test
     public void whenSortDescWithMissedDepartments() {
-        List<String> input = Arrays.asList(
+        List<String> input = List.of(
                 "K1/SK1/SSK1",
                 "K1/SK1/SSK2",
                 "K2/SK1",
@@ -110,7 +110,7 @@ public class DepartmentTest {
                 "K2/SK1/SSK2",
                 "K2/SK1/SSK1"
         );
-        List<String> expected = Arrays.asList(
+        List<String> expected = List.of(
                 "K2/SK1",
                 "K2/SK1/SSK1",
                 "K2/SK1/SSK2",
