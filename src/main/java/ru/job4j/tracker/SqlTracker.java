5a4ebe52cd710043f2e19f79744c85d40f02d3d9
Sergei Chernov <goldengrail@gmail.com>
2024-02-06 22:16:36 +0600

    2. Трекер SQL [#1734 #477996]

--- src/main/java/ru/job4j/tracker/SqlTracker.java
+++ src/main/java/ru/job4j/tracker/SqlTracker.java
@@ -2,7 +2,6 @@ package ru.job4j.tracker;
 
 import java.io.InputStream;
 import java.sql.*;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -34,7 +33,7 @@ public class SqlTracker implements Store {
         }
     }
 
-    private Item setIdFromDB(ResultSet resultSet, Item item) throws SQLException {
+    private void setIdFromDB(ResultSet resultSet, Item item) throws SQLException {
         int id = -1;
         if (resultSet.next()) {
             id = resultSet.getInt(1);
@@ -42,7 +41,6 @@ public class SqlTracker implements Store {
         if (id != -1) {
             item.setId(id);
         }
-        return item;
     }
 
     @Override
@@ -52,7 +50,7 @@ public class SqlTracker implements Store {
             statement.setTimestamp(2, Timestamp.valueOf(item.getCreated()));
             statement.execute();
             try (ResultSet resultSet = statement.getGeneratedKeys()) {
-                item = setIdFromDB(resultSet, item);
+                setIdFromDB(resultSet, item);
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -65,11 +63,15 @@ public class SqlTracker implements Store {
     @Override
     public boolean replace(int id, Item item) {
         boolean result = false;
-        try (PreparedStatement statement = connection.prepareStatement("update items set name = ? where id = ?")) {
+        try (PreparedStatement statement = connection.prepareStatement("update items set name = ? where id = ?", Statement.RETURN_GENERATED_KEYS)) {
             statement.setString(1, item.getName());
             statement.setInt(2, id);
-            if (statement.execute()) {
+            statement.execute();
+            try (ResultSet resultSet = statement.getGeneratedKeys()) {
+                setIdFromDB(resultSet, item);
                 result = true;
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         } catch (SQLException e) {
             e.printStackTrace();
